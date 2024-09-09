03deed405e0200ef583a6272376cd9d478f22aa7
fabricio.junior <fabriciojuniorsc@gmail.com>
2024-09-09 19:48:39 -0300

    Criação do projeto

---
+++ mini-banco/src/main/java/com/senac/mini_banco/MiniBancoApplication.java
@@ -0,0 +1,13 @@
+package com.senac.mini_banco;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MiniBancoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MiniBancoApplication.class, args);
+	}
+
+}
