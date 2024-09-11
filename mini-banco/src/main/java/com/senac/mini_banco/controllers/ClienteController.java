45d0fbfdcf863cf12b641300530985be5188d3a5
fabricio.junior <fabriciojuniorsc@gmail.com>
2024-09-11 20:16:43 -0300

    Método PUT, DELETE e GET por id

--- mini-banco/src/main/java/com/senac/mini_banco/controllers/ClienteController.java
+++ mini-banco/src/main/java/com/senac/mini_banco/controllers/ClienteController.java
@@ -2,10 +2,12 @@ package com.senac.mini_banco.controllers;
 
 import com.senac.mini_banco.model.Cliente;
 import com.senac.mini_banco.repositories.ClienteRepository;
+import jakarta.persistence.EntityNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.Optional;
 
 @RestController
 @RequestMapping("clientes")
@@ -23,11 +25,39 @@ public class ClienteController {
         return clienteRepository.findAll();
     }
 
+    @GetMapping("{id}")
+    public Cliente findById(@PathVariable("id") Integer id) {
+        Optional<Cliente> cliente = clienteRepository.findById(id);
+
+        if (cliente.isPresent()) {
+            return cliente.get();
+        } else {
+            throw new EntityNotFoundException("Cliente não encontrado.");
+        }
+    }
+
     @PostMapping
     public Cliente save(@RequestBody Cliente cliente) {
         clienteRepository.save(cliente);
         return cliente;
     }
 
+    @PutMapping("{id}")
+    public Cliente update(@PathVariable("id") Integer id, @RequestBody Cliente clienteRequisicao) {
+        Optional<Cliente> clienteOpt = clienteRepository.findById(id);
 
+        if (clienteOpt.isPresent()) {
+            Cliente clienteSalvo = clienteOpt.get();
+            clienteSalvo.setNome(clienteRequisicao.getNome());
+
+            return clienteRepository.save(clienteSalvo);
+        } else {
+            throw new EntityNotFoundException("Cliente não encontrado.");
+        }
+    }
+
+    @DeleteMapping("{id}")
+    public void delete(@PathVariable("id") Integer id) {
+        clienteRepository.deleteById(id);
+    }
 }
